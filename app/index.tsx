4c0d6e948037bbc32f3383c927a79e1f0ab6c52b
WMBYBE <williambbrouillette@gmail.com>
2025-03-27 13:05:30 -0500

    page navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {StyleSheet, Button, View, Text, Alert, TextInput} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
 
 export default function Index() {
     const [text, onChangeText] = React.useState('Username');
+    const router = useRouter();
   return (
     <View
       style={{
@@ -13,7 +15,9 @@ export default function Index() {
       }}
     >
 
-      <Text>Odassey</Text>
+      <Button onPress={() => router.push('/login')} title="Login"></Button>
+
+      <Text>Odaseey</Text>
     </View>
   );
 }
